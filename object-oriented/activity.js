728d0ec27cc28a5e82c2eab14dc3fef8b3cc4769
Melanie Allmond <melanie.allmond@gmail.com>
2023-09-12 21:22:59 -0400

    assignment complete

--- object-oriented/activity.js
+++ object-oriented/activity.js
@@ -9,10 +9,14 @@ function forEach(array, callback){
 
 //Exercise 3 define forEach function to use the array it is invoked on, instead of passing array as an arguement
 function forEach(callback){
-    
+    for(let i = 0; i < this.length; i++){
+        const element = this[i];
+        callback(element)
+    }
 }
-//Exercise 4: following instructions in canvas allow arrays to use your function
 
+//Exercise 4: following instructions in canvas allow arrays to use your function
+Array.prototype.forEach = forEach
 let numbers = [1,2,3]
 numbers.forEach(function(e){
     console.log("iteration:", e)
